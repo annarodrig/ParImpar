97a7da58d9b346686962548c316018b8450c0289
Anna Beatriz <annab_oliver@hotmail.com>
2022-04-01 20:25:38 -0300

    Finaliza exercicio de decisão

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿//  Cria váriavel numérica
+
+int numeroDigitado;
+
+Console.Write("Número digitado:");
+numeroDigitado = Convert.ToInt32 (Console.ReadLine());
+
+// Cria váriavel verdadeira ou falsa (bolleano)
+
+bool numeroEhPar = (numeroDigitado % 2) == 0;
+
+if (numeroEhPar)
+{     
+    Console.WriteLine ($"{numeroDigitado} é par");
+
+}
+
+else 
+{
+    Console.WriteLine($"{numeroDigitado} é ímpar");
+}
